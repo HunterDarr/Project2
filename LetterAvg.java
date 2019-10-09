40c9b317496c906b0a0f2e8dd5b8fe79bb10bf9b
HunterDarr <Hunter.darr.one@gmail.com>
2019-10-09 17:42:06 -0500

    Major changes to LetterAvg in the LetterAvg constructor. it now does the sorting and most of the computing

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,12 +1,26 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LetterAvg {
 	
-	private int numberOfStations;
+	private int numberOfStations = 0;
+	
+	private ArrayList<String> matchingStids = new ArrayList<String>();
+	
+	private String[] newArrayReference;
 	
 	
 	
-	public LetterAvg(char averageLetter)   {
+	public LetterAvg(char averageLetter) throws IOException   {
+		PosAvg array = new PosAvg("ARRY");
+		newArrayReference = array.stationIdList;
 		
+		for (int i = 0; i < newArrayReference.length; i++ )   {   //May need to copy read() method instead of using a public array
+			if (newArrayReference[i].charAt(0) == averageLetter )   {
+				numberOfStations = numberOfStations + 1;
+				matchingStids.add(newArrayReference[i]);
+			}
+		}
 		
 	}
 	
